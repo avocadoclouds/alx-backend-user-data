bbb1447f05bc7e6eacc2b965eeb302592143119e
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-22 00:19:17 +0300

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """encrypt passwords with bcrypt"""
+from xmlrpc.client import Boolean
 import bcrypt
 
 
@@ -9,3 +10,11 @@ def hash_password(password: str) -> bytes:
     hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    returns a boolean
+    to validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
