38eb45245e67a583eb0562bc15477e298dd89113
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-27 02:10:38 +0300

    task 9 & 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 
 from api.v1.auth.auth import Auth
 from base64 import b64decode
-from typing import TypeVar
+from typing import TypeVar, Tuple
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -53,3 +54,57 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('utf-8')
         except Exception as e:
             None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        """
+        returns the user email and password from the Base64 decoded value.
+        This method must return 2 values
+        Return None, None if decoded_base64_authorization_header is None
+        Return None, None if decoded_base64_authorization_header-
+         is not a string
+        Return None, None if decoded_base64_authorization_header-
+         doesn’t contain:
+        Otherwise, return the user email and the user password-
+         these 2 values must be separated by a :
+        You can assume decoded_base64_authorization_header-
+         will contain only one :
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) != None:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        values = decoded_base64_authorization_header.split(":")
+        return values[0], ":".join(values[1:])
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        returns the User instance based on his email and password.
+        - Return None if user_email is None or not a string
+        - Return None if user_pwd is None or not a string
+        - Return None if your database (file) doesn’t contain-
+          any User instance with email equal to user_email-
+          you should use the class method search of the User- 
+          to lookup the list of users based on their email. Don’t forget-
+          to test all cases: “what if there is no user in DB?”, etc.
+        - Return None if user_pwd is not the password of the User-
+          instance found - you must use the method is_valid_password of User
+        - Otherwise, return the User instance.
+        """
+
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            return None
