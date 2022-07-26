374ab258b2f317bf5f95d17d47ea07cc47e8e85c
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-27 02:22:03 +0300

    task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -108,3 +108,20 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        overloads Auth and retrieves the User instance for a request:
+        You must use authorization_header
+        You must use extract_base64_authorization_header
+        You must use decode_base64_authorization_header
+        You must use extract_user_credentials
+        You must use user_object_from_credentials
+        """
+        header = self.authorization_header(request)
+        base64 = self.extract_base64_authorization_header(header)
+        decoded = self.decode_base64_authorization_header(base64)
+        credentials = self.extract_user_credentials(decoded)
+        user = user = self.user_object_from_credentials(*credentials)
+
+        return user
