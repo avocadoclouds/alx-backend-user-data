434d09e22933e7745e5aa7b778c6b9c6fa3db619
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-27 01:14:59 +0300

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,23 @@ class BasicAuth(Auth):
     """
     Inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        returns the Base64 part of the Authorization header-
+        for a Basic Authentication:
+            Return None if authorization_header is None
+            Return None if authorization_header is not a string
+            Return None if authorization_header doesn’t start by Basic-
+            (with a space at the end)
+            Otherwise, return the value after Basic (after the space)
+            You can assume authorization_header contains only one Basic
+        """
+
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+        if authorization_header[0:6] == "Basic ":
+            return authorization_header[6:]
