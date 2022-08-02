40f733156f97afa10db9165f342dd96f1544f4e3
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-03 02:47:33 +0300

    task 0 fix issue

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,11 +2,9 @@
 """
 creating a datebase model and a table
 """
-import sqlalchemy
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-engine = create_engine('mysql:///:Info:', echo=True)
+
 
 Base = declarative_base()
 
@@ -18,8 +16,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String)
-    reset_token = Column(String)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
     
