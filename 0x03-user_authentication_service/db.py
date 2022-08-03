7ff98adb3533b9639aae430dc04204b75241e556
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-03 03:34:19 +0300

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -40,3 +41,21 @@ class DB():
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        - Returns the first row found in the users table,
+        as filtered by the method’s input arguments.
+        - method takes in arbitrary keyword arguments, which
+        is used for filtering
+        """
+
+        # query from the database
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+
+        if user is None:
+            raise NoResultFound
+        return user
