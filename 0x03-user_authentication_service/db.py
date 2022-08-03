0c98350002145adf0fd0aa2a6b014ea39050db69
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-03 03:47:37 +0300

    task 2: revert to try block

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -50,12 +50,10 @@ class DB():
         is used for filtering
         """
 
-        # check kwargs
-        if kwargs is None:
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
             raise InvalidRequestError
-
-        # query the data
-        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
         return user
