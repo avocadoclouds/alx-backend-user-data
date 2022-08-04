63c9c5a34606a21ac7a43c18bc09e3f029b3f570
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-05 02:10:19 +0300

    task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -88,5 +88,25 @@ def get_profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """
+    Request is expected to contain an "email" field.
+    If the email is not registered, respond with a 403 status code.
+    Otherwise, generate a token and respond with a 200 HTTP status,
+    and a JSON payload
+    """
+    user_request = request.form
+    user_email = user_request.get('email')
+    is_registered = AUTH.create_session(user_email)
+
+    if not is_registered:
+        abort(403)
+
+    token = AUTH.get_reset_password_token(user_email)
+    message = {"email": user_email, "reset_token": token}
+    return jsonify(message)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="7000")
