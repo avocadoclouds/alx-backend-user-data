7e19be42bc7b6894a0a93c453711be99a04bfe71
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-08-04 13:18:42 +0300

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Auth Model"""
 
-from asyncio.events import BaseDefaultEventLoopPolicy
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
 from user import User
 from db import DB
 
@@ -15,3 +15,28 @@ def _hash_password(password: str) -> bytes:
 
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Returns a new user, if email is used it raises value error
+        if not, the password is hashed and saved to db
+        """
+
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+
+            return user
+
+        else:
+            raise ValueError(f'User {email} already exists')
